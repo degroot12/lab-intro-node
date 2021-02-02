bf2f31728d723155e04701a4ca72869973816839
Thomas de Groot <degroot12@gmail.com>
2021-02-02 17:23:42 +0100

    all done

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length++
+  }
 
-  get(pos) {}
+  get(pos) {
+    let getItem = this.items[pos]
+    if(!getItem){
+      throw new Error('OutOfBounds')
+    }
+    return getItem
+  }
 
-  max() {}
+  max() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.items.length-1]
+    
+  }
+  min() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+      return this.items[0]
+  }
 
-  min() {}
+  sum() {
+    if(this.length === 0){
+      return 0
+    }
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return this.items.reduce(reducer)
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return (this.items.reduce(reducer))/this.length
+  }
 }
 
 module.exports = SortedList;
